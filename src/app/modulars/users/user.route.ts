f5c707faadf5789d94e57effe5bd8e02ed030813
jahid45780 <jahidhossain88899@gmail.com>
2026-01-09 01:29:55 +0600

    add the verifiy token

--- src/app/modulars/users/user.route.ts
+++ src/app/modulars/users/user.route.ts
@@ -1,7 +1,10 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { userController } from "./user.controller";
 import { validateRequest } from "../../middleware/validateRequest";
 import { createUserZodSchema } from "./user.validation";
+import  jwt, { JwtPayload }  from 'jsonwebtoken';
+import AppError from "../../errorHerplrs/appError";
+
 
 
 
@@ -9,6 +12,32 @@ const router = Router()
 
 router.post('/register', validateRequest(createUserZodSchema), userController.createUser)
 
-router.get('/all-users', userController.getAllUsers )
+router.get('/all-users', async (req:Request, res:Response, next:NextFunction)=>{
+      
+    
+try {
+    const accessToken = req.headers.authorization
+
+    if(!accessToken){
+        throw new AppError(403, "Not received token")
+    }
+
+    const verifiedToken = jwt.verify(accessToken,"SECRET")
+
+    if(!verifiedToken){
+         throw new AppError(403, " Your  Not authorization")
+    }
+
+    if((verifiedToken as JwtPayload).role !=="ADMIN"){
+         throw new AppError(403, "you not permitted to view this  route")
+    }
+    
+    next()
+} catch (error) {
+    next(error)
+}
+
+
+}, userController.getAllUsers )
 
  export const userRouter = router;
