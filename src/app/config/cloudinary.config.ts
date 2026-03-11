3796073f5fec0eb1898c06a163efa190dee5ae64
jahid45780 <jahidhossain88899@gmail.com>
2026-03-12 01:40:39 +0600

    add the cludinaly img delete fn

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,10 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { v2 as cloudinary } from "cloudinary";
 import { envVers } from "./env";
+import AppError from "../errorHerplrs/appError";
 
 cloudinary.config({
     cloud_name:envVers.CLOUDINARY.CLOUDINARY_CLOUD_NAME,
     api_key:envVers.CLOUDINARY.CLOUDINARY_API_KEY,
     api_secret:envVers.CLOUDINARY.CLOUDINARY_API_SECRET
 })
+
+
+export const deleteImageFromCLoudinary = async (url:string)=>{
+    try {
+        const regex = /\/v\d+\/(.*?)\.(jpg|jpeg|png|gif|webp)$/i;
+
+    const match = url.match(regex);
+    if(match && match[1]){
+        const public_id = match[1];
+        await cloudinary.uploader.destroy(public_id)
+        console.log(`File ${public_id} is deleted from cloudinary`);
+    }
+    } catch (error:any) {
+      throw new AppError(401, "Cloudinary image deletion failed", error.message)  
+    }
+}
 
 export const cloudinaryUpload = cloudinary
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -2,14 +2,26 @@
 import { NextFunction, Request, Response } from "express";
 import { envVers } from "../config/env";
 import AppError from "../errorHerplrs/appError";
+import { deleteImageFromCLoudinary } from "../config/cloudinary.config";
 
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const globalErrorHandler = (err:any, req:Request, res:Response, next:NextFunction)=>{
+export const globalErrorHandler = async (err:any, req:Request, res:Response, next:NextFunction)=>{
 
 
     let statusCode = 500;
     let message = 'something went wrong'
+
+     console.log({ file: req.files });
+    if (req.file) {
+        await deleteImageFromCLoudinary(req.file.path)
+    }
+
+    if (req.files && Array.isArray(req.files) && req.files.length) {
+        const imageUrls = (req.files as Express.Multer.File[]).map(file => file.path)
+
+        await Promise.all(imageUrls.map(url => deleteImageFromCLoudinary(url)))
+    }
 
     const errorSources: { path: string; message: string }[] = [];
 
