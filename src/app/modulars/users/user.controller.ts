ef2888b355161eeaf92185627da2fec72e319686
jahid45780 <jahidhossain88899@gmail.com>
2026-07-28 03:45:07 +0600

    modify api

--- src/app/modulars/users/user.controller.ts
+++ src/app/modulars/users/user.controller.ts
@@ -79,6 +79,11 @@ const updateUser = catchAsync(async(req:Request, res:Response, next:NextFunction
   // const token = req.headers.authorization
   // const verifiedToken = verifyToken(token as string, envVers.BCRYPT_SALT_ROUND) as JwtPayload
    const payload = req.body
+
+       if (req.file) {
+      payload.picture = req.file.path; 
+    }
+
    const user  =  await userService.updateUser(userId, payload, verifiedToken as JwtPayload )
   
 
--- src/app/modulars/users/user.route.ts
+++ src/app/modulars/users/user.route.ts
@@ -19,7 +19,10 @@ router.post('/register',
 router.get('/all-users', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), userController.getAllUsers )
 router.get("/me", checkAuth(...Object.values(Role)), userController.getMe)
 router.get("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), userController.getSingleUser)
-router.patch("/:id", validateRequest(UpdateUserZodSchema), checkAuth(...Object.values(Role)), userController.updateUser)
+router.patch("/:id",
+multerUpload.single("file"),
+validateRequest(UpdateUserZodSchema), checkAuth(...Object.values(Role)),
+userController.updateUser)
 router.patch("/change-role/:id", checkAuth(Role.ADMIN), userController.changeUserRole)
 router.delete("/:userId", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), userController.deleteUser )
 
